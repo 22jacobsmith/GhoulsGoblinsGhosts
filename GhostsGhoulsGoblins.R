6d4ebeb9e6f53b245bead2e5db8fe43d1ec4a658
Jacob Smith <22jacobsmith@gmail.com>
2023-11-03 13:32:38 -0600

    Fitted several models w multinomial adjustments

--- GhostsGhoulsGoblins.R
+++ GhostsGhoulsGoblins.R
@@ -3,11 +3,12 @@
 library(tidyverse)
 library(tidymodels)
 library(vroom)
+library(embed)
 # read in data
 train <- vroom("train.csv")
 test <- vroom("test.csv")
 imputed_set <- vroom("trainWithMissingValues.csv")
-head(train)
+#head(train)
 
 # impute the missing values in the data set
 
@@ -59,3 +60,242 @@ prep <- prep(impute_recipe_bag)
 baked <- bake(prep, new_data = imputed_set)
 
 rmse_vec(train[is.na(imputed_set)], baked[is.na(imputed_set)])
+
+
+## MODEL FITTING ##
+## RANDOM FOREST
+
+rf_recipe <-
+  recipe(type~., data=train) %>%
+  step_normalize(all_numeric_predictors())
+
+rf_mod <- rand_forest(min_n = tune(), mtry = tune()) %>%
+  set_engine('ranger') %>%
+  set_mode('classification')
+
+
+rf_wf <-
+  workflow() %>%
+  add_recipe(rf_recipe) %>%
+  add_model(rf_mod)
+
+
+
+## set up a tuning grid
+tuning_grid <-
+  grid_regular(mtry(range = c(1,6)),
+               min_n(),
+               levels = 6)
+
+## split into folds
+folds <- vfold_cv(train, v = 5, repeats = 1)
+
+# run cv
+
+CV_results <-
+  rf_wf %>%
+  tune_grid(resamples = folds,
+            grid = tuning_grid,
+            metrics = metric_set(accuracy))
+
+# find best tuning parm values
+
+best_tune <-
+  CV_results %>%
+  select_best("accuracy")
+
+# finalize wf and get preds
+
+final_wf <-
+  rf_wf %>%
+  finalize_workflow(best_tune) %>%
+  fit(data = train)
+
+rf_preds <-
+  final_wf %>%
+  predict(new_data = test, type = 'class')
+
+# prepare and export preds to csv for kaggle
+
+rf_output <- tibble(id = test$id, type = rf_preds$.pred_class)
+
+vroom_write(rf_output, "GGG_RFPreds.csv", delim = ",")
+
+## KNN
+
+knn_recipe <-
+  recipe(type~., data=train) %>%
+  step_normalize(all_numeric_predictors())
+
+knn_mod <- nearest_neighbor(neighbors = tune(), dist_power = tune()) %>%
+  set_engine('kknn') %>%
+  set_mode('classification')
+
+
+knn_wf <-
+  workflow() %>%
+  add_recipe(knn_recipe) %>%
+  add_model(knn_mod)
+
+
+## set up a tuning grid
+tuning_grid <-
+  grid_regular(neighbors(),
+               dist_power(),
+               levels = 6)
+
+## split into folds
+folds <- vfold_cv(train, v = 5, repeats = 1)
+
+# run cv
+
+CV_results <-
+  knn_wf %>%
+  tune_grid(resamples = folds,
+            grid = tuning_grid,
+            metrics = metric_set(accuracy))
+
+# find best tuning parm values
+
+best_tune <-
+  CV_results %>%
+  select_best("accuracy")
+
+# finalize wf and get preds
+
+final_wf <-
+  knn_wf %>%
+  finalize_workflow(best_tune) %>%
+  fit(data = train)
+
+knn_preds <-
+  final_wf %>%
+  predict(new_data = test, type = 'class')
+
+# prepare and export preds to csv for kaggle
+
+knn_output <- tibble(id = test$id, type = knn_preds$.pred_class)
+
+vroom_write(knn_output, "GGG_KNNPreds.csv", delim = ",")
+
+
+## NAIVE BAYES
+
+library(discrim)
+
+nb_recipe <-
+  recipe(type~., data=train) %>%
+  step_dummy(all_nominal_predictors()) %>%
+  step_normalize(all_numeric_predictors()) %>%
+  step_pca(all_predictors(), threshold = .99)
+
+nb_mod <- naive_Bayes(Laplace = tune(), smoothness = tune()) %>%
+  set_engine('naivebayes') %>%
+  set_mode('classification')
+
+
+nb_wf <-
+  workflow() %>%
+  add_recipe(nb_recipe) %>%
+  add_model(nb_mod)
+
+
+## set up a tuning grid
+tuning_grid <-
+  grid_regular(Laplace(),
+               smoothness(),
+               levels = 6)
+
+## split into folds
+folds <- vfold_cv(train, v = 5, repeats = 1)
+
+# run cv
+
+CV_results <-
+  nb_wf %>%
+  tune_grid(resamples = folds,
+            grid = tuning_grid,
+            metrics = metric_set(accuracy))
+
+# find best tuning parm values
+
+best_tune <-
+  CV_results %>%
+  select_best("accuracy")
+
+# finalize wf and get preds
+
+final_wf <-
+  nb_wf %>%
+  finalize_workflow(best_tune) %>%
+  fit(data = train)
+
+nb_preds <-
+  final_wf %>%
+  predict(new_data = test, type = 'class')
+
+# prepare and export preds to csv for kaggle
+
+nb_output <- tibble(id = test$id, type = nb_preds$.pred_class)
+
+vroom_write(nb_output, "GGG_NBPreds.csv", delim = ",")
+
+### SVM
+
+
+svm_recipe <- recipe(type~., data=train) %>%
+   step_normalize(all_numeric_predictors())
+
+
+
+
+svmRadial <- svm_rbf(rbf_sigma=tune(), cost=tune()) %>% # set or tune
+  set_mode("classification") %>%
+  set_engine("kernlab")
+
+
+svm_wf <- 
+  workflow() %>%
+  add_recipe(svm_recipe) %>%
+  add_model(svmRadial)
+
+## set up a tuning grid
+tuning_grid <-
+  grid_regular(rbf_sigma(),
+               cost(),
+               levels = 20)
+
+## split into folds
+folds <- vfold_cv(train, v = 5, repeats = 1)
+
+# run cv
+
+CV_results <-
+  svm_wf %>%
+  tune_grid(resamples = folds,
+            grid = tuning_grid,
+            metrics = metric_set(accuracy))
+
+# find best tuning parm values
+
+best_tune <-
+  CV_results %>%
+  select_best("accuracy")
+
+# finalize wf and get preds
+
+final_wf <-
+  svm_wf %>%
+  finalize_workflow(best_tune) %>%
+  fit(data = train)
+
+svm_preds <-
+  final_wf %>%
+  predict(new_data = test, type = "class")
+
+# prepare and export preds to csv for kaggle
+
+svm_output <- tibble(id = test$id, type = svm_preds$.pred_class)
+
+
+vroom_write(svm_output, "GGG_SVMPreds.csv", delim = ",")
